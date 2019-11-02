fa029938d959e9eb43db5c1bced58d31d83a981b
Manan Gupta <54077213+MananGupta00@users.noreply.github.com>
2019-11-02 14:51:25 +0530

    Add files via upload

--- cal1.js
+++ cal1.js
@@ -1,6 +1,40 @@
 window.onload=function(){
 /////////////////////////////////////////// for popmotion	
     		
+const contnr = popmotion.styler(document.querySelector('.container'));
+
+	
+
+	popmotion.tween({
+ 
+    	from: {
+ 
+        	scale: -1,
+
+		opacity: 0,
+
+		y:-200,
+
+		x: -500
+
+   	 	},
+
+   	 to: {
+ 
+    	    	scale: 1,
+
+		opacity: 1,
+
+		y:00,
+
+		x:00
+ 
+   		},
+
+    	duration: 1000
+	}).start(contnr.set);
+
+
 ///////////////////////////////////////////
 	
 	
@@ -73,7 +107,42 @@ function Number(num){
 	}	
 }
 
+function presentdiv(value){
+	const doc= document.querySelector('.present');
+	doc.innerText=value;
+	const press = popmotion.styler(doc);
+
+
+	
+
+	popmotion.tween({
+ 
+    	from: {
+ 
+        	
+		opacity: 1,
+
+		y: 00,
+
+
+   	 	},
+
+   	 to: {
+ 
+    	    	
+
+		opacity: 0,
+
+		y:100
+   		},
+
+    	duration: 2000
+	}).start(press.set);
+}
+
 function differtype(value){
+
+	presentdiv(value);
 	const num= parseInt(value);
 	
 	if(isNaN(num))
