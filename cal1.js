bb7d8e3aa9e048ba7a2c57e8b1656a300519cf43
Manan Gupta <54077213+MananGupta00@users.noreply.github.com>
2019-11-07 21:12:49 +0530

    Update cal1.js

--- cal1.js
+++ cal1.js
@@ -1,3 +1,27 @@
+				const ball = document.querySelector('.container');
+				const divStyler = styler(ball);
+				const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+				listen(ball, 'mousedown touchstart')
+					.start((e) => {
+						e.preventDefault();
+						pointer(ballXY.get()).start(ballXY);
+					});
+
+				listen(document, 'mouseup touchend')
+					.start(() => {
+						spring({
+							from: ballXY.get(),
+							velocity: ballXY.getVelocity(),
+							to: { x: 0, y: 0 },
+							stiffness: 200,
+							// mass: 1,
+							// damping: 10
+						}).start(ballXY);
+					});
+
+
+
 window.onload=function(){
 /////////////////////////////////////////// for popmotion	
  		
